ba5ee7222496f317b9c8180edeafc1de7ab20b0c
Talimeq <boren1996@wp.pl>
2025-01-24 18:51:53 +0000

    Rendering a shroom

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,26 @@
 use bevy::prelude::*;
 
 // Component examples
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
 
 // A simple system
 fn hello_world()
 {
-    println!("hello world!");
+
 }
 
 // Component initialization example
-fn add_people(mut commands: Commands)
+fn add_people(mut commands: Commands, asset_server: Res<AssetServer>)
 {
-    commands.spawn((Person,Name("Elaina Proctor".to_string())));
-    commands.spawn((Person,Name("Renzh Bow".to_string())));
-    commands.spawn((Person,Name("Zang Renzh".to_string())));
+    commands.spawn(Camera2d);
+    commands.spawn((Sprite::from_image( asset_server.load("sprites/mushroom.png")), Transform::from_xyz(256.,0.,0.)));
 }
-
-// Query example
-fn greet_people(query: Query<&Name,With<Person>>)
-{
-    for name in &query
-    {
-        println!("hello {}!", name.0);
-    }
-}
-
-// Mutable query example
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
-        }
-    }
-}
-
 
 fn main()
 {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world,(update_people, greet_people).chain()))
+        .add_systems(Update, hello_world)
         .run();
 }
 
