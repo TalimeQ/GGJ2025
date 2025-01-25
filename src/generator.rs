3be495fb308da8197a1e96fcbe3ba4f09646c2b4
Grzegorz 'Envy' Perzanowski <grzegorz@envysoftware.pl>
2025-01-25 01:32:15 +0100

    generator

--- src/generator.rs
+++ src/generator.rs
@@ -1,8 +1,10 @@
+use std::collections::HashMap;
 use bevy::asset::{Assets, Handle};
 use bevy::color::{Color, Srgba};
 use bevy::image::Image;
 use bevy::prelude::*;
 use bevy_asset_loader::asset_collection::AssetCollection;
+use crate::{Cell, CellDefinition, CellType};
 
 #[derive(Resource, AssetCollection)]
 pub struct MapSource {
@@ -11,18 +13,51 @@ pub struct MapSource {
 }
 
 const ENEMY_CELL: &str = "ac3232"; //RED
+const EMPTY_CELL: &str = "ffffff"; //WHITE
 
-pub fn prepare_map(mut commands: Commands, map_source: Res<MapSource>, images: Res<Assets<Image>>) {
 
-    if let Some(map) = images.get(&map_source.map_tiles) {
-        for i in 0..map.width() {
-            for j in 0..map.height() {
-                let color : Srgba = map.get_color_at(i, j).unwrap().into();
-                if color == Srgba::hex(&ENEMY_CELL).unwrap()
-                {
-                    println!("i:{i} j:{j} IS ENEMY!", i = i, j = j );
+pub fn initialize_grid(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    map_source: Res<MapSource>,
+    images: Res<Assets<Image>>)
+{
+    commands.spawn(Camera2d);
+    let map = images.get(&map_source.map_tiles).unwrap();
+
+    let sprite_size :f32 = 16.;
+
+    let x_amount : u32 =  map.width();
+    let y_amount : u32 =  map.height();
+
+    let x_offset : f32 = x_amount as f32 / 2. * sprite_size;
+    let y_offset : f32 = y_amount as f32 / 2. * sprite_size;
+
+    let mut cell_hashmap  = HashMap::new();
+    cell_hashmap.insert(ENEMY_CELL, CellDefinition{cell_type : CellType::BasicEnemy, sprite_path: "sprites/EvilBubble.png".to_string()});
+    cell_hashmap.insert(EMPTY_CELL, CellDefinition{cell_type : CellType::Empty, sprite_path: "sprites/NoSprite.png".to_string()});
+
+    let mut rng = rand::thread_rng();
+
+    for i in 0..  y_amount
+    {
+        for j in 0.. x_amount
+        {
+            let x: f32 = j as f32 * sprite_size - x_offset;
+            let y: f32 = -(i as f32 * sprite_size - y_offset);
+            let z: f32 = 0.;
+
+            let color : Srgba = map.get_color_at(j, i).unwrap().into();
+            match cell_hashmap.get(&color.to_hex().as_str().to_lowercase()[1..])
+            {
+                Some(cell) => {
+                    commands.spawn((Sprite::from_image( asset_server.load(&cell.sprite_path)),
+                                    Transform::from_xyz(x,y,z),
+                                    Cell{cell_type : cell.cell_type.clone()}));
                 }
+                None => {}
             }
         }
     }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 mod generator;
 mod game_state;
-
 mod input;
 
 use bevy::prelude::*;
-use bevy::window::WindowMode;
-use std::collections::HashMap;
 use bevy_asset_loader::prelude::*;
 use crate::game_state::*;
 use crate::generator::*;
@@ -37,48 +34,6 @@ struct Cell
     cell_type: CellType
 }
 
-// Component initialization example
-fn initialize_grid(mut commands: Commands, asset_server: Res<AssetServer>)
-{
-    commands.spawn(Camera2d);
-
-    let sprite_size :f32 = 16.;
-
-    let x_amount : i32 =  40;
-    let y_amount : i32 =  20;
-
-    let x_offset : f32 = x_amount as f32 / 2. * sprite_size;
-    let y_offset : f32 = y_amount as f32 / 2. * sprite_size;
-
-    let mut cell_hashmap  = HashMap::new();
-
-    cell_hashmap.insert(1, CellDefinition{cell_type : CellType::Empty, sprite_path: "sprites/EvilBubble.png".to_string()});
-
-
-    let mut rng = rand::thread_rng();
-
-    for i in 0..  y_amount
-    {
-        for j in 0.. x_amount
-        {
-            let x: f32 = j as f32 * sprite_size - x_offset;
-            let y: f32 = i as f32 * sprite_size - y_offset;
-            let z: f32 = 0.;
-
-            match cell_hashmap.get(&1)
-            {
-                Some(cell) => {
-                    commands.spawn((Sprite::from_image( asset_server.load(&cell.sprite_path)),
-                                    Transform::from_xyz(x,y,z),
-                                    Cell{cell_type : cell.cell_type.clone()}));
-                }
-                None => {}
-            }
-        }
-    }
-
-}
-
 fn main()
 {
     App::new()
@@ -88,8 +43,7 @@ fn main()
             LoadingState::new(GameStates::AssetLoading)
                 .continue_to_state(GameStates::Next)
                 .load_collection::<MapSource>())
-        .add_systems(Startup, initialize_grid)
-        .add_systems(OnEnter(GameStates::Next), prepare_map)
+        .add_systems(OnEnter(GameStates::Next), initialize_grid)
         .add_systems(Update, (input::grab_mouse, input::cursor_position))
         .run();
 }
